c616377dc61bde95a446f5537e2a84674ca47a49
Andreas Finger <webmaster@mediafinger.com>
2015-08-20 01:09:23 +0200

    Add configuration file

---
+++ config.rb
@@ -0,0 +1,6 @@
+class Config
+  SLACK_CHANNEL_NAME = "Ruby Web Devs"
+  SLACK_CHANNEL_URL = "https://rubywebdev.slack.com"
+  # generate your API token at https://api.slack.com/
+  SLACK_CHANNEL_TOKEN = ENV["SLACK_CHANNEL_RUBYWEBDEV_API_TOKEN"]
+end
