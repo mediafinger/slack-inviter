7ba4975f76d5995d3b80bc7fdf47cc3bb1192f45
Andreas Finger <webmaster@mediafinger.com>
2021-05-25 23:17:37 +0200

    Update config to read all values from ENV

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Config
-  SLACK_TEAM_NAME = "Ruby Web Devs"
-  SLACK_TEAM_URL = "https://rubywebdev.slack.com"
+  SLACK_TEAM_NAME = ENV.fetch("SLACK_TEAM_NAME", "Ruby Web Devs")
+  SLACK_TEAM_URL = ENV.fetch("SLACK_TEAM_URL", "https://rubywebdev.slack.com")
   # generate your API token at https://api.slack.com/
-  SLACK_TEAM_TOKEN = ENV["SLACK_TEAM_RUBYWEBDEV_API_TOKEN"]
+  SLACK_TEAM_TOKEN = ENV.fetch("SLACK_TEAM_TOKEN", "test")
 end
