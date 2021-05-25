e075cbf6a6fb55d5d9a65778ff145b01adddf894
Andreas Finger <webmaster@mediafinger.com>
2021-05-25 22:51:11 +0200

    Replace rest-client by chimera_http_client

--- models/inviter.rb
+++ models/inviter.rb
@@ -10,7 +10,7 @@ class Inviter
     return { success: false, message: "We need your email address!" } if email.nil? || email.empty?
     return { success: false, message: "Please tell us your name." }   if first_name.nil? || first_name.empty?
 
-    result = post_request(email, first_name, options)
+    result = post_request(email: email, first_name: first_name, options: options)
 
     if result[:success]
       { success: true, message: "Done for <b>#{email}</b> - check your emails now!" }
@@ -21,25 +21,27 @@ class Inviter
 
   private
 
-  def post_request(email, first_name, _options)
-    api_result = RestClient.post(full_url, post_params(email, first_name))
-    hash = JSON.parse(api_result)
+  def post_request(email:, first_name:, options:)
+    endpoint = "users.admin.invite"
+    response = connection.post(endpoint, body: post_params(email, first_name, options))
+    hash = response.parsed_body
 
     # The Slack API returns {"ok"=>true} if everything worked
     # and {"ok"=>false, "error"=>"some_reason"} if an error occured
     { success: hash["ok"], error: hash["error"] }
   end
 
-  def full_url
-    "#{@url}/api/users.admin.invite"
+  def connection
+    ChimeraHttpClient::Connection.new(base_url: "#{@url}/api/")
   end
 
-  def post_params(email, first_name)
+  def post_params(email, first_name, options)
     {
       email: email,
       first_name: first_name,
       token: @token,
       set_active: true,
+      options: options,
     }
   end
 end
